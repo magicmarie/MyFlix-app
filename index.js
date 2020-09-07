9d5a9248d8a4bba431bae1627451f0a7bb430c77
magicmarie <natukunda162@gmail.com>
2020-09-07 09:07:50 +0300

    web server frameworks and error handling

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+  morgan = require('morgan');
+  bodyParser = require('body-parser'),
+  methodOverride = require('method-override');
+
+const app = express();
+
+app.use(morgan('common'));
+app.use(express.static('public'));
+
+let movies = [
+  {
+    title: 'Harry Potter and the Sorcerer\'s Stone',
+    author: 'J.K. Rowling'
+  },
+  {
+    title: 'Lord of the Rings',
+    author: 'J.R.R. Tolkien'
+  },
+  {
+    title: 'Twilight',
+    author: 'Stephanie Meyer'
+  }
+];
+
+// error handling
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// GET requests
+app.get('/', (req, res) => {
+  res.send('Welcome to movie api!');
+});
+
+app.get('/documentation', (req, res) => {
+  res.sendFile('public/documentation.html', { root: __dirname });
+});
+
+app.get('/movies', (req, res) => {
+  res.json(movies);
+});
+
+// listen for requests
+app.listen(8080, () =>
+  console.log('Your app is listening on port 8080.'));
